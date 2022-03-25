889202534d1acfe74689d83bb9876b81105054e6
Griffin 4065 <tkashaf@dundee.ac.uk>
2022-03-25 01:20:45 +0000

    Training data classified from Alabama, 1/50

--- Workspace/TweetParser/ManualClassifier.py
+++ Workspace/TweetParser/ManualClassifier.py
@@ -1,23 +1,32 @@
 import os
 
-state = "Alabama"
+state = "Alaska"
 
+count = 0
 with open('../Data/FullArchiveCleaned/{}.txt'.format(state), 'r') as inputFile:
-    #Reads the first line
-    line = inputFile.readline()
-    #Grabs only the ID
-    ID = line[4:23]
-    print(ID)
-    #Reads the second line
-    line = inputFile.readline()
-    #Grabs only the tweet (drops the "Text: " from the start)
-    Tweet = line[6:len(line)]
-    print(Tweet)
-    #Reads an empty line to get ready for the next set of tweets, each tweet occupies 3 lines. ID/Tweet/Blank Line
-    inputFile.readline()
+    while count < 100:
+        #Reads the first line
+        line = inputFile.readline()
+        #Grabs only the ID
+        ID = line[4:23]
+        print(ID)
+        #Reads the second line
+        line = inputFile.readline()
+        #Grabs only the tweet (drops the "Text: " from the start)
+        Tweet = line[6:len(line)]
+        print(Tweet)
+        #Reads an empty line to get ready for the next set of tweets, each tweet occupies 3 lines. ID/Tweet/Blank Line
+        inputFile.readline()
 
-    with open('Positive/{}-{}.txt'.format(state, ID), 'w') as outputFile:
-        outputFile.write(Tweet)
+        manualClassification = input('Enter Classification (p or n): ')
 
-    with open('Negative/{}-{}.txt'.format(state, ID), 'w') as outputFile:
-        outputFile.write(Tweet)
+        if manualClassification == 'p':
+            with open('Positive/{}-{}.txt'.format(state, ID), 'w') as outputFile:
+                outputFile.write(Tweet)
+        elif manualClassification == 'n':
+            with open('Negative/{}-{}.txt'.format(state, ID), 'w') as outputFile:
+                outputFile.write(Tweet)
+        else:
+            manualClassification = input('Classification not recognized enter ONLY p or n: ')
+            
+        count = count + 1
