a6292fa6ebd4c28a088a02d7f2b395c854f0e776
Griffin 4065 <tkashaf@dundee.ac.uk>
2022-03-26 21:51:52 +0000

    Changed classification methodology, updated script, 0/50

--- Workspace/TweetParser/ManualClassifier.py
+++ Workspace/TweetParser/ManualClassifier.py
@@ -1,10 +1,24 @@
 import os
 import sys
 
-state = sys.argv[1]
 
 count = 1
 with open('../Data/FullArchiveCleaned/{}.txt'.format(state), 'r') as inputFile:
+
+    def manualClassfier(inputClassification, tweetState, tweetID, tweetText):
+        if inputClassification == 'p':
+            with open('Positive/{}-{}.txt'.format(tweetState, tweetID), 'w') as outputFile:
+                outputFile.write(tweetText)
+        elif inputClassification == 'n':
+            with open('Negative/{}-{}.txt'.format(tweetState, tweetID), 'w') as outputFile:
+                outputFile.write(tweetText)
+        elif inputClassification == 'm':
+            with open('Neutral/{}-{}.txt'.format(tweetState, tweetID), 'w') as outputFile:
+                outputFile.write(tweetText)
+        else:
+            inputClassification = input('Invalid Classification, try again (p/n/m): ')
+            manualClassfier(inputClassification, tweetState, tweetID, tweetText)
+
     while count < 101:
         #Reads the first line
         line = inputFile.readline()
@@ -19,15 +33,8 @@ with open('../Data/FullArchiveCleaned/{}.txt'.format(state), 'r') as inputFile:
         #Reads an empty line to get ready for the next set of tweets, each tweet occupies 3 lines. ID/Tweet/Blank Line
         inputFile.readline()
 
-        manualClassification = input('Enter Classification (p or n): ')
+        manualClassification = input('Enter Classification (p/n/m): ')
 
-        if manualClassification == 'p':
-            with open('Positive/{}-{}.txt'.format(state, ID), 'w') as outputFile:
-                outputFile.write(Tweet)
-        elif manualClassification == 'n':
-            with open('Negative/{}-{}.txt'.format(state, ID), 'w') as outputFile:
-                outputFile.write(Tweet)
-        else:
-            manualClassification = input('Classification not recognized enter ONLY p or n: ')
+        manualClassfier(manualClassification, sys.argv[1], ID, Tweet)
             
         count = count + 1
